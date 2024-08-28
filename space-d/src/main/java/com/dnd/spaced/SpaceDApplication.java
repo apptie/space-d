a7ab0f8eb6cfd61c3f79d3546fc2e162fb7aa850
apptie <appti@naver.com>
2024-08-28 20:02:38 +0900

    feat: 애플리케이션 초기 세팅

---
+++ space-d/src/main/java/com/dnd/spaced/SpaceDApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.spaced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceDApplication.class, args);
+	}
+
+}
