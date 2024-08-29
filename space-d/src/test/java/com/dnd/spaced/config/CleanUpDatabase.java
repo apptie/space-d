ca2c94bcc97770351150ec74140d58f89cf9d813
apptie <appti@naver.com>
2024-08-29 10:59:26 +0900

    test: DB 격리 애노테이션 내용 분리

--- space-d/src/test/java/com/dnd/spaced/config/CleanUpDatabase.java
+++ space-d/src/test/java/com/dnd/spaced/config/CleanUpDatabase.java
@@ -2,13 +2,8 @@ package com.dnd.spaced.config;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Retention(RetentionPolicy.RUNTIME)
 @TestExecutionListeners(value = {CleanUpDatabaseListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 public @interface CleanUpDatabase {
