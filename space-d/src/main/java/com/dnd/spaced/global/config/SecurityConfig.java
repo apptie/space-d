d80dac42319c38f808dd8ec7d94dc850e2b93769
apptie <appti@naver.com>
2024-09-01 21:33:38 +0900

    feat: 회원 가입 시 서비스에서 무작위로 지정할 회원 정보 관련 기능 추가

--- space-d/src/main/java/com/dnd/spaced/global/config/SecurityConfig.java
+++ space-d/src/main/java/com/dnd/spaced/global/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.dnd.spaced.global.config;
 
+import com.dnd.spaced.global.config.properties.NicknameProperties;
+import com.dnd.spaced.global.config.properties.ProfileImageProperties;
 import com.dnd.spaced.global.config.properties.TokenProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(value = {TokenProperties.class})
+@RequiredArgsConstructor
+@EnableConfigurationProperties(
+        value = {TokenProperties.class, NicknameProperties.class, ProfileImageProperties.class}
+)
 public class SecurityConfig {
 }
---
+++ space-d/src/main/java/com/dnd/spaced/global/config/properties/NicknameProperties.java
@@ -0,0 +1,29 @@
+package com.dnd.spaced.global.config.properties;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("app.profile.nickname")
+@RequiredArgsConstructor
+public class NicknameProperties {
+
+    private final List<String> adjective;
+    private final List<String> planet;
+    private final String format;
+
+    public String generate() {
+        int adjectiveIndex = generateRandomIndex(adjective.size());
+        int planetIndex = generateRandomIndex(planet.size());
+
+        return adjective.get(adjectiveIndex).concat(planet.get(planetIndex));
+    }
+
+    public String format(String nickname, long count) {
+        return nickname + String.format(format, count);
+    }
+
+    private static int generateRandomIndex(int range) {
+        return (int) (Math.random() * range);
+    }
+}
