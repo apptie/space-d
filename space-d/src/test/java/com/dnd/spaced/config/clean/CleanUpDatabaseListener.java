c754f2a76052a5862d077407910652970c6cd351
apptie <appti@naver.com>
2024-09-01 21:05:50 +0900

    test: 통합 테스트 시 데이터베이스 초기화 로직 변경

--- space-d/src/test/java/com/dnd/spaced/config/clean/CleanUpDatabaseListener.java
+++ space-d/src/test/java/com/dnd/spaced/config/clean/CleanUpDatabaseListener.java
@@ -13,9 +13,9 @@ public class CleanUpDatabaseListener extends AbstractTestExecutionListener {
     @Override
     public void beforeTestMethod(TestContext testContext) {
         EntityManager em = findEntityManager(testContext);
-        List<String> tableNames = calculateTableNames(em);
+        List<EntityType<?>> entityTypes = findEntityTypes(em);
 
-        clean(em, tableNames);
+        clean(em, entityTypes);
     }
 
     private EntityManager findEntityManager(TestContext testContext) {
@@ -23,47 +23,52 @@ public class CleanUpDatabaseListener extends AbstractTestExecutionListener {
                           .getBean(EntityManager.class);
     }
 
-    private List<String> calculateTableNames(EntityManager em) {
+    private List<EntityType<?>> findEntityTypes(EntityManager em) {
         return em.getMetamodel()
                  .getEntities()
                  .stream()
                  .filter(entityType -> entityType.getJavaType().getAnnotation(Entity.class) != null)
-                 .map(this::calculateTableName)
                  .toList();
+    }
+
+    private void clean(EntityManager em, List<EntityType<?>> entityTypes) {
+        em.flush();
+
+        StringBuilder sb = new StringBuilder("SET REFERENTIAL_INTEGRITY FALSE;");
+
+        for (EntityType<?> entityType : entityTypes) {
+            String tableName = calculateTableName(entityType);
+
+            sb.append("TRUNCATE TABLE ")
+              .append(tableName)
+              .append(";");
+
+            if (Long.class.equals(entityType.getIdType().getJavaType())) {
+                sb.append("ALTER TABLE ")
+                  .append(tableName)
+                  .append(" ALTER COLUMN id RESTART WITH 1;");
+            }
+        }
+
+        sb.append("SET REFERENTIAL_INTEGRITY TRUE;");
+
+        em.createNativeQuery(sb.toString())
+          .executeUpdate();
     }
 
     private String calculateTableName(EntityType<?> entityType) {
         Table tableAnnotation = entityType.getJavaType().getAnnotation(Table.class);
 
         if (tableAnnotation != null) {
-            return tableAnnotation.name().toLowerCase();
+            return tableAnnotation.name()
+                                  .toLowerCase();
         }
 
         return convertToSnakeCase(entityType.getName());
     }
 
     private String convertToSnakeCase(String input) {
-        return input.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
-    }
-
-
-    private void clean(EntityManager em, List<String> tableNames) {
-        em.flush();
-
-        StringBuilder sb = new StringBuilder("SET REFERENTIAL_INTEGRITY FALSE;");
-
-        for (final String tableName : tableNames) {
-            sb.append("TRUNCATE TABLE ")
-              .append(tableName)
-              .append(";");
-
-            sb.append("ALTER TABLE ")
-              .append(tableName)
-              .append(" ALTER COLUMN id RESTART WITH 1;");
-        }
-
-        sb.append("SET REFERENTIAL_INTEGRITY TRUE;");
-
-        em.createNativeQuery(sb.toString()).executeUpdate();
+        return input.replaceAll("([a-z])([A-Z])", "$1_$2")
+                    .toLowerCase();
     }
 }
