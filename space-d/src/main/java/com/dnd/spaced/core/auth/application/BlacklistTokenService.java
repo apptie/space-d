ae14353de99f63bb7ef926aff9e502bbec665ab0
apptie <appti@naver.com>
2024-09-03 23:56:28 +0900

    feat: 블랙리스트 토큰 등록 기능 추가

--- space-d/src/main/java/com/dnd/spaced/core/auth/application/BlacklistTokenService.java
+++ space-d/src/main/java/com/dnd/spaced/core/auth/application/BlacklistTokenService.java
@@ -1,7 +1,10 @@
 package com.dnd.spaced.core.auth.application;
 
+import com.dnd.spaced.core.auth.domain.BlacklistToken;
 import com.dnd.spaced.core.auth.domain.repository.BlacklistTokenRepository;
 import com.dnd.spaced.core.auth.domain.PrivateClaims;
+import java.time.Clock;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BlacklistTokenService {
 
+    private final Clock clock;
     private final BlacklistTokenRepository blacklistTokenRepository;
 
     public boolean isBlockedToken(PrivateClaims privateClaims) {
         return blacklistTokenRepository.findBy(privateClaims.accountId())
                                        .map(blacklistToken -> blacklistToken.isBlacklistToken(privateClaims.issuedAt()))
                                        .orElse(Boolean.FALSE);
+    }
+
+    @Transactional
+    public void register(String accountId) {
+        BlacklistToken blacklistToken = new BlacklistToken(accountId, LocalDateTime.now(clock));
+
+        blacklistTokenRepository.save(blacklistToken);
     }
 }
