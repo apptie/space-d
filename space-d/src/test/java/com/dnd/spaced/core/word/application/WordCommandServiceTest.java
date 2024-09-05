0e2243484ed4e456db509eb998939704dcbc0e0b
apptie <appti@naver.com>
2024-09-05 20:41:25 +0900

    feat: 용어 관련 Command 서비스 기능 추가

---
+++ space-d/src/test/java/com/dnd/spaced/core/word/application/WordCommandServiceTest.java
@@ -0,0 +1,65 @@
+package com.dnd.spaced.core.word.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.dnd.spaced.config.clean.annotation.CleanUpDatabase;
+import com.dnd.spaced.core.word.application.dto.request.SaveWordDto;
+import com.dnd.spaced.core.word.application.dto.request.SaveWordDto.PronunciationInfoDto;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@CleanUpDatabase
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class WordCommandServiceTest {
+
+    @Autowired
+    WordCommandService wordCommandService;
+
+    @Test
+    void saveWord_메서드는_유효한_CreateWordDto를_전달하면_Word를_초기화하고_영속화한다() {
+        // given
+        List<PronunciationInfoDto> pronunciationInfoDtos = List.of(new PronunciationInfoDto("어써라이제이션", "한글 발음"));
+        List<String> examples = List.of("example");
+        SaveWordDto saveWordDto = new SaveWordDto(
+                "name",
+                "word meaning",
+                "개발",
+                pronunciationInfoDtos,
+                examples
+        );
+
+        // when
+        Long actual = wordCommandService.saveWord(saveWordDto);
+
+        // then
+        assertThat(actual).isPositive();
+    }
+
+    @Test
+    void updateWordExample_메서드는_지정한_id에_해당하는_WordExample의_example을_전달한_example로_변환한다() {
+        // when & then
+        assertDoesNotThrow(() -> wordCommandService.updateWordExample(1L, "changed word example"));
+    }
+
+    @Test
+    void deleteWordExample_메서드는_지정한_id에_해당하는_WordExample을_삭제한다() {
+        // when & then
+        assertDoesNotThrow(() -> wordCommandService.deleteWordExample(1L));
+    }
+
+    @Test
+    void deletePronunciation_메서드는_지정한_id에_해당하는_Pronunciation을_삭제한다() {
+        // when & then
+        assertDoesNotThrow(() -> wordCommandService.deletePronunciation(1L));
+    }
+}
