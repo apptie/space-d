8bc696caf0eb18ce6b1b9f05984538fe0ad027d1
apptie <appti@naver.com>
2024-10-06 17:29:38 +0900

    fix: 좋아요 개수 버퍼에 불필요한 연산 제거

--- space-d/src/main/java/com/dnd/spaced/core/like/infrastructure/LikeCountBuffer.java
+++ space-d/src/main/java/com/dnd/spaced/core/like/infrastructure/LikeCountBuffer.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
@@ -15,18 +14,15 @@ public class LikeCountBuffer {
 
     private static final int FLUSH_THRESHOLD = 100;
 
-    private final AtomicInteger likeEventCount = new AtomicInteger(0);
     private final AtomicReference<Map<LikeCountIdentifier, Integer>> currentBuffer = new AtomicReference<>(new ConcurrentHashMap<>());
     private final Consumer<Map<LikeCountIdentifier, Integer>> cacheUpdateCallback;
     private final Executor asyncCommentLikeCountExecutor;
 
     public void addLikeCount(LikeCountIdentifier identifier) {
-        currentBuffer.get()
-                     .merge(identifier, 1, Integer::sum);
+        Map<LikeCountIdentifier, Integer> buffer = currentBuffer.get();
+        buffer.merge(identifier, 1, Integer::sum);
 
-        likeEventCount.incrementAndGet();
-
-        if (likeEventCount.compareAndSet(FLUSH_THRESHOLD, 0)) {
+        if (buffer.size() == FLUSH_THRESHOLD) {
             flushBuffer();
         }
     }
