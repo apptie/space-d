dfa67efd1373560acfee4d8406cd1e7b98efa846
apptie <appti@naver.com>
2024-11-02 13:37:49 +0900

    test: Experience 테스트 케이스 변경

--- space-d/src/test/java/com/dnd/spaced/core/account/domain/ExperienceTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/domain/ExperienceTest.java
@@ -4,26 +4,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.dnd.spaced.core.account.domain.exception.InvalidExperienceException;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ExperienceTest {
 
-    @Test
-    void 경력과_일치하는_도메인을_반환한다() {
-        // given
-        Experience betweenFirstSecond = Experience.BETWEEN_FIRST_SECOND;
+    private static Stream<Arguments> findByTestArguments() {
+        return Stream.of(
+                Arguments.of("1년 차 미만", Experience.UNDER_FIRST),
+                Arguments.of("1~2년 차", Experience.BETWEEN_FIRST_SECOND),
+                Arguments.of("2~3년 차", Experience.BETWEEN_SECOND_THIRD),
+                Arguments.of("3~4년 차", Experience.BETWEEN_THIRD_FOURTH),
+                Arguments.of("4~5년 차", Experience.BETWEEN_FOURTH_FIFTH),
+                Arguments.of("5년 차 이상", Experience.OVER_FIFTH),
+                Arguments.of("비공개", Experience.BLIND)
+        );
+    }
 
+    @ParameterizedTest(name = "경력이 {0}일 때 {1}을 반환한다")
+    @MethodSource("findByTestArguments")
+    void 경력과_일치하는_도메인을_반환한다(String experienceName, Experience expected) {
         // when
-        Experience actual = Experience.findBy(betweenFirstSecond.getName());
+        Experience actual = Experience.findBy(experienceName);
 
         // then
-        assertThat(actual).isEqualTo(betweenFirstSecond);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest
