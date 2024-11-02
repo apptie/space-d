2fd71668c43eac0a3689d92791fcc31c02fd42a8
apptie <appti@naver.com>
2024-11-02 13:26:01 +0900

    test: Company findBy 테스트 케이스 변경

--- space-d/src/test/java/com/dnd/spaced/core/account/domain/CompanyTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/domain/CompanyTest.java
@@ -4,26 +4,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.dnd.spaced.core.account.domain.exception.InvalidCompanyException;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CompanyTest {
 
-    @Test
-    void 회사명과_일치하는_도메인을_반환한다() {
-        // given
-        Company blind = Company.BLIND;
+    private static Stream<Arguments> findByTestArguments() {
+        return Stream.of(
+                Arguments.of("대기업", Company.MAJOR),
+                Arguments.of("중견기업", Company.MIDSIZE),
+                Arguments.of("중소기업", Company.SMALL),
+                Arguments.of("스타트업", Company.STARTUP),
+                Arguments.of("외국계", Company.FOREIGN),
+                Arguments.of("취준생/인턴", Company.JOB_HUNTER_INTERN),
+                Arguments.of("비공개", Company.BLIND)
+        );
+    }
 
+    @ParameterizedTest(name = "회사명이 {0}일 때 {1}을 반환한다")
+    @MethodSource("findByTestArguments")
+    void 회사명과_일치하는_도메인을_반환한다(String companyName, Company expected) {
         // when
-        Company actual = Company.findBy(blind.getName());
+        Company actual = Company.findBy(companyName);
 
         // then
-        assertThat(actual).isEqualTo(blind);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest
