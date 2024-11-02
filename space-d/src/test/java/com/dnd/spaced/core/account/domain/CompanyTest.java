fa22762b54a89821e27c82c491a1cd7b590bee77
apptie <appti@naver.com>
2024-11-02 13:22:31 +0900

    test: Company 테스트 메서드 네이밍 변경

--- space-d/src/test/java/com/dnd/spaced/core/account/domain/CompanyTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/domain/CompanyTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 class CompanyTest {
 
     @Test
-    void findBy_메서드는_유효한_name을_전달하면_name에_맞는_Company를_반환한다() {
+    void 회사명과_일치하는_도메인을_반환한다() {
         // given
         Company blind = Company.BLIND;
 
@@ -28,7 +28,7 @@ class CompanyTest {
 
     @ParameterizedTest
     @NullAndEmptySource
-    void findBy_메서드는_유효하지_않은_name을_전달하면_InvalidCompanyException_예외가_발생한다(String invalidName) {
+    void 유효한_회사명이_아닌_경우_예외가_발생한다(String invalidName) {
         // when & then
         assertThatThrownBy(() -> Company.findBy(invalidName))
                 .isInstanceOf(InvalidCompanyException.class)
