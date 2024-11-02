e576756b260fe57dcdd0ae2d122e82a9da120b70
apptie <appti@naver.com>
2024-11-02 17:46:47 +0900

    test: AdminWordService 테스트 케이스 추가

--- space-d/src/test/java/com/dnd/spaced/core/admin/application/AdminWordServiceTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/admin/application/AdminWordServiceTest.java
@@ -1,11 +1,15 @@
 package com.dnd.spaced.core.admin.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import com.dnd.spaced.config.clean.annotation.CleanUpDatabase;
 import com.dnd.spaced.core.admin.application.dto.request.SaveWordDto;
 import com.dnd.spaced.core.admin.application.dto.request.SaveWordDto.PronunciationInfoDto;
+import com.dnd.spaced.core.admin.application.exception.PronunciationDeletionNotAllowedException;
+import com.dnd.spaced.core.admin.application.exception.UnexpectedUpdateWordExampleCountException;
+import com.dnd.spaced.core.admin.application.exception.WordExampleDeletionNotAllowedException;
 import com.dnd.spaced.core.word.domain.Word;
 import com.dnd.spaced.core.word.domain.repository.WordRepository;
 import java.util.List;
@@ -79,6 +83,18 @@ class AdminWordServiceTest {
     }
 
     @Test
+    void 용어_예문_변경_시_유효한_용어_예문_식별자가_아닌_경우_예외가_발생한다() {
+        // when & then
+        assertThatThrownBy(
+                () -> adminWordService.updateWordExample(
+                        1L,
+                        "이 기능은 일반 사용자의 Authorization 범위를 벗어나므로, 관리자 권한이 필요합니다."
+                )
+        ).isInstanceOf(UnexpectedUpdateWordExampleCountException.class)
+         .hasMessage("용어 예문이 정상적으로 변경되지 않았습니다.");
+    }
+
+    @Test
     void 용어_예문을_삭제한다() {
         // given
         List<PronunciationInfoDto> pronunciationInfoDtos = List.of(new PronunciationInfoDto("어써라이제이션", "한글 발음"));
@@ -105,6 +121,32 @@ class AdminWordServiceTest {
     }
 
     @Test
+    void 용어_예문_삭제_시_용어_예문의_개수가_최소치라면_예외가_발생한다() {
+        // given
+        List<PronunciationInfoDto> pronunciationInfoDtos = List.of(new PronunciationInfoDto("어써라이제이션", "한글 발음"));
+        List<String> examples = List.of(
+                "게시글 삭제는 작성자와 관리자만 Authorization이 있도록 구현했습니다."
+        );
+        SaveWordDto saveWordDto = new SaveWordDto(
+                "Authorization",
+                "Authorization(권한 부여)은 인증된 사용자가 특정 리소스나 기능에 접근할 수 있는 권한이 있는지를 확인하고 제어하는 보안 메커니즘",
+                "개발",
+                pronunciationInfoDtos,
+                examples
+        );
+
+        Long wordId = adminWordService.saveWord(saveWordDto);
+        Word word = wordRepository.findBy(wordId)
+                                  .get();
+
+        // when & then
+        assertThatThrownBy(
+                () -> adminWordService.deleteWordExample(word.getId(), word.getWordExamples().get(0).getId())
+        ).isInstanceOf(WordExampleDeletionNotAllowedException.class)
+         .hasMessage("해당 용어의 예문 개수가 최소치입니다.");
+    }
+
+    @Test
     void 용어_발음_정보를_삭제한다() {
         // given
         List<PronunciationInfoDto> pronunciationInfoDtos = List.of(
@@ -128,5 +170,31 @@ class AdminWordServiceTest {
         assertDoesNotThrow(
                 () -> adminWordService.deletePronunciation(wordId, word.getPronunciations().get(0).getId())
         );
+    }
+
+    @Test
+    void 용어_발음_정보_삭제_시_용어_발음_정보의_개수가_최소치라면_예외가_발생한다() {
+        // given
+        List<PronunciationInfoDto> pronunciationInfoDtos = List.of(
+                new PronunciationInfoDto("어써라이제이션", "한글 발음")
+        );
+        List<String> examples = List.of("게시글 삭제는 작성자와 관리자만 Authorization이 있도록 구현했습니다.");
+        SaveWordDto saveWordDto = new SaveWordDto(
+                "Authorization",
+                "Authorization(권한 부여)은 인증된 사용자가 특정 리소스나 기능에 접근할 수 있는 권한이 있는지를 확인하고 제어하는 보안 메커니즘",
+                "개발",
+                pronunciationInfoDtos,
+                examples
+        );
+
+        Long wordId = adminWordService.saveWord(saveWordDto);
+        Word word = wordRepository.findBy(wordId)
+                                  .get();
+
+        // when & then
+        assertThatThrownBy(
+                () -> adminWordService.deletePronunciation(wordId, word.getPronunciations().get(0).getId())
+        ).isInstanceOf(PronunciationDeletionNotAllowedException.class)
+         .hasMessage("해당 용어의 발음 정보 개수가 최소치입니다.");
     }
 }
