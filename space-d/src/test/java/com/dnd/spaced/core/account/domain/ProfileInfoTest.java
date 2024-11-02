8fb90a0fa2b86d1bcd9f12067ea6c9392d8c9ff8
apptie <appti@naver.com>
2024-11-02 13:58:05 +0900

    test: ProfileInfo 테스트 리팩터링

--- space-d/src/test/java/com/dnd/spaced/core/account/domain/ProfileInfoTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/domain/ProfileInfoTest.java
@@ -21,23 +21,30 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 class ProfileInfoTest {
 
     @Test
-    void 생성자는_유효한_nickname_profileImage를_전달하면_ProfileInfo를_초기화하고_반환한다() {
+    void 프로필_정보를_초기화한다() {
         // when & then
         assertDoesNotThrow(() -> new ProfileInfo("nickname", "profileInfo"));
     }
 
+    private static Stream<Arguments> constructorTestWithInvalidNickname() {
+        return Stream.of(
+                Arguments.of((Object) null), Arguments.of(""), Arguments.of("  "),
+                Arguments.of("1234"), Arguments.of("12345678901")
+        );
+    }
+
     @ParameterizedTest(name = "닉네임이 {0}일 때 예외가 발생한다")
     @MethodSource("constructorTestWithInvalidNickname")
-    void 생성자는_유효하지_않은_nickname을_전달하면_InvalidNicknameException_예외가_발생한다(String invalidNickname) {
+    void 프로필_정보를_초기화할_때_유효한_닉네임이_아니라면_예외가_발생한다(String invalidNickname) {
         // when & then
         assertThatThrownBy(() -> new ProfileInfo(invalidNickname, "profileInfo"))
                 .isInstanceOf(InvalidNicknameException.class)
                 .hasMessage("닉네임은 최소 5글자 이상, 최대 10글자 이하여야 합니다.");
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "프로필 이미지가 {0}일 때 예외가 발생한다")
     @NullAndEmptySource
-    void 생성자는_유효하지_않은_profileImage를_전달하면_InvalidProfileImageException_예외가_발생한다(String invalidProfileImage) {
+    void 프로필_정보를_초기화할_때_유효한_프로필_이미지가_아니라면_예외가_발생한다(String invalidProfileImage) {
         // when & then
         assertThatThrownBy(() -> new ProfileInfo("nickname", invalidProfileImage))
                 .isInstanceOf(InvalidProfileImageException.class)
@@ -45,7 +52,7 @@ class ProfileInfoTest {
     }
 
     @Test
-    void changeProfile_메서드는_유효한_changedNickname_changedProfileImage를_전달하면_회원_정보를_변경한다() {
+    void 프로필_정보를_변경한다() {
         // given
         ProfileInfo profileInfo = new ProfileInfo("nickname", "profileImage");
 
@@ -62,9 +69,9 @@ class ProfileInfoTest {
         );
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "프로필 이미지가 {0}일 때 예외가 발생한다")
     @NullAndEmptySource
-    void changeProfile_메서드는_유효하지_않은_profileImage를_전달하면_InvalidProfileImageException_예외가_발생한다(String invalidProfileImage) {
+    void 프로필_정보를_변경할_때_유효한_프로필_이미지가_아니라면_예외가_발생한다(String invalidProfileImage) {
         // given
         ProfileInfo profileInfo = new ProfileInfo("nickname", "profileImage");
 
@@ -74,9 +81,16 @@ class ProfileInfoTest {
                 .hasMessage("프로필 이미지 정보는 null이거나 비어 있을 수 없습니다.");
     }
 
+    private static Stream<Arguments> changeProfileInfoTestWithInvalidNickname() {
+        return Stream.of(
+                Arguments.of((Object) null), Arguments.of(""), Arguments.of("  "),
+                Arguments.of("1234"), Arguments.of("12345678901")
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("changeProfileInfoTestWithInvalidNickname")
-    void changeProfile_메서드는_유효하지_않은_nickname을_전달하면_InvalidNicknameException_예외가_발생한다(String invalidNickname) {
+    void 프로필_정보를_변경할_때_유효한_닉네임이_아니라면_예외가_발생한다(String invalidNickname) {
         // given
         ProfileInfo profileInfo = new ProfileInfo("nickname", "profileImage");
 
@@ -84,19 +98,5 @@ class ProfileInfoTest {
         assertThatThrownBy(() -> profileInfo.changeProfileInfo(invalidNickname, "profileImage"))
                 .isInstanceOf(InvalidNicknameException.class)
                 .hasMessage("닉네임은 최소 5글자 이상, 최대 10글자 이하여야 합니다.");
-    }
-
-    private static Stream<Arguments> constructorTestWithInvalidNickname() {
-        return Stream.of(
-                Arguments.of((Object) null), Arguments.of(""), Arguments.of("  "),
-                Arguments.of("aaaa"), Arguments.of("aaaaaaaaaaa")
-        );
-    }
-
-    private static Stream<Arguments> changeProfileInfoTestWithInvalidNickname() {
-        return Stream.of(
-                Arguments.of((Object) null), Arguments.of(""), Arguments.of("  "),
-                Arguments.of("aaaa"), Arguments.of("aaaaaaaaaaa")
-        );
     }
 }
