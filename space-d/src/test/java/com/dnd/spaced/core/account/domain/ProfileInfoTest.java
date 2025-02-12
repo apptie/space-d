bdf1c31653f6d665e419c12df74c396b9fa8769a
apptie <appti@naver.com>
2025-02-12 20:15:30 +0900

    test: ProfileInfo 테스트 리팩터링

--- space-d/src/test/java/com/dnd/spaced/core/account/domain/ProfileInfoTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/domain/ProfileInfoTest.java
@@ -23,30 +23,33 @@ class ProfileInfoTest {
     @Test
     void 프로필_정보를_초기화한다() {
         // when & then
-        assertDoesNotThrow(() -> new ProfileInfo("nickname", "profileInfo"));
+        assertDoesNotThrow(() -> new ProfileInfo("재빠른지구001", "earth.png"));
     }
 
     private static Stream<Arguments> constructorTestWithInvalidNickname() {
         return Stream.of(
-                Arguments.of((Object) null), Arguments.of(""), Arguments.of("  "),
-                Arguments.of("1234"), Arguments.of("12345678901")
+                Arguments.of((Object) null),
+                Arguments.of(""),
+                Arguments.of("  "),
+                Arguments.of("1234"),
+                Arguments.of("12345678901")
         );
     }
 
     @ParameterizedTest(name = "닉네임이 {0}일 때 예외가 발생한다")
     @MethodSource("constructorTestWithInvalidNickname")
-    void 프로필_정보를_초기화할_때_유효한_닉네임이_아니라면_프로필_정보를_초기화할_수_없다(String invalidNickname) {
+    void 프로필_정보를_초기화할_때_유효한_길이의_닉네임이_아니라면_프로필_정보를_초기화할_수_없다(String invalidNickname) {
         // when & then
-        assertThatThrownBy(() -> new ProfileInfo(invalidNickname, "profileInfo"))
+        assertThatThrownBy(() -> new ProfileInfo(invalidNickname, "earth.png"))
                 .isInstanceOf(InvalidNicknameException.class)
                 .hasMessage("닉네임은 최소 5글자 이상, 최대 10글자 이하여야 합니다.");
     }
 
     @ParameterizedTest(name = "프로필 이미지가 {0}일 때 예외가 발생한다")
     @NullAndEmptySource
-    void 프로필_정보를_초기화할_때_유효한_프로필_이미지가_아니라면_프로필_정보를_초기화할_수_없다(String invalidProfileImage) {
+    void 프로필_정보를_초기화할_때_비어_있는_프로필_이미지_경로라면_프로필_정보를_초기화할_수_없다(String invalidProfileImage) {
         // when & then
-        assertThatThrownBy(() -> new ProfileInfo("nickname", invalidProfileImage))
+        assertThatThrownBy(() -> new ProfileInfo("행복한지구001", invalidProfileImage))
                 .isInstanceOf(InvalidProfileImageException.class)
                 .hasMessage("프로필 이미지 정보는 null이거나 비어 있을 수 없습니다.");
     }
@@ -54,11 +57,11 @@ class ProfileInfoTest {
     @Test
     void 프로필_정보를_변경한다() {
         // given
-        ProfileInfo profileInfo = new ProfileInfo("nickname", "profileImage");
+        ProfileInfo profileInfo = new ProfileInfo("재빠른지구001", "earth.png");
 
         // when
-        String changedNickname = "changeNick";
-        String changedProfileImage = "changeProfileImage";
+        String changedNickname = "행복한화성001";
+        String changedProfileImage = "mars.png";
 
         profileInfo.changeProfileInfo(changedNickname, changedProfileImage);
 
@@ -71,26 +74,29 @@ class ProfileInfoTest {
 
     @ParameterizedTest(name = "프로필 이미지가 {0}일 때 예외가 발생한다")
     @NullAndEmptySource
-    void 프로필_정보를_변경할_때_유효한_프로필_이미지가_아니라면_프로필_쩡보를_변경할_수_없다(String invalidProfileImage) {
+    void 프로필_정보를_변경할_때_비어_있는_프로필_이미지_경로라면_프로필_정보를_변경할_수_없다(String invalidProfileImage) {
         // given
-        ProfileInfo profileInfo = new ProfileInfo("nickname", "profileImage");
+        ProfileInfo profileInfo = new ProfileInfo("재빠른지구001", "earth.png");
 
         // when & then
-        assertThatThrownBy(() -> profileInfo.changeProfileInfo("nickname", invalidProfileImage))
+        assertThatThrownBy(() -> profileInfo.changeProfileInfo("행복한화성001", invalidProfileImage))
                 .isInstanceOf(InvalidProfileImageException.class)
                 .hasMessage("프로필 이미지 정보는 null이거나 비어 있을 수 없습니다.");
     }
 
     private static Stream<Arguments> changeProfileInfoTestWithInvalidNickname() {
         return Stream.of(
-                Arguments.of((Object) null), Arguments.of(""), Arguments.of("  "),
-                Arguments.of("1234"), Arguments.of("12345678901")
+                Arguments.of((Object) null),
+                Arguments.of(""),
+                Arguments.of("  "),
+                Arguments.of("1234"),
+                Arguments.of("12345678901")
         );
     }
 
     @ParameterizedTest(name = "닉네임이 {0}일 때 예외가 발생한다")
     @MethodSource("changeProfileInfoTestWithInvalidNickname")
-    void 프로필_정보를_변경할_때_유효한_닉네임이_아니라면_프로필_쩡보를_변경할_수_없다(String invalidNickname) {
+    void 프로필_정보를_변경할_때_유효한_닉네임_길이가_아니라면_프로필_정보를_변경할_수_없다(String invalidNickname) {
         // given
         ProfileInfo profileInfo = new ProfileInfo("nickname", "profileImage");
 
