836a77bf0d11b61bec0aa3ac2a3d555d0b582e8b
apptie <appti@naver.com>
2025-02-15 15:00:53 +0900

    test: BlacklistToken 테스트 리팩터링

--- space-d/src/test/java/com/dnd/spaced/core/auth/domain/BlacklistTokenTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/auth/domain/BlacklistTokenTest.java
@@ -31,7 +31,7 @@ class BlacklistTokenTest {
     @MethodSource("isBlacklistTokenTestWithRegisteredAtAndIssuedAtAndExpected")
     void 등록_일자를_통해_블랙리스트에_등록된_토큰인지_판단한다(String targetRegisteredAt, String targetIssuedAt, boolean expected) {
         // given
-        String id = "email";
+        String id = "user1@naver.com";
         LocalDateTime registeredAt = LocalDateTimeFixture.from(targetRegisteredAt);
         LocalDateTime issuedAt = LocalDateTimeFixture.from(targetIssuedAt);
 
@@ -47,12 +47,12 @@ class BlacklistTokenTest {
     @Test
     void 블랙리스트_토큰에_등록될_회원_식별자와_등록_일자로_블랙리스트_토큰을_초기화한다() {
         // when & then
-        assertDoesNotThrow(() -> new BlacklistToken("id", LocalDateTime.now()));
+        assertDoesNotThrow(() -> new BlacklistToken("user1@naver.com", LocalDateTime.now()));
     }
 
     @ParameterizedTest(name = "id가 {0}일 때 예외가 발생한다")
     @NullAndEmptySource
-    void 블랙리스트_토큰_초기화_시_유효한_회원_식별자가_아니라면_초기화_할_수_없다(String invalidId) {
+    void 블랙리스트_토큰_초기화_시_비어_있는_식별자라면_블랙리스트_토큰을_초기화_할_수_없다(String invalidId) {
         // when & then
         assertThatThrownBy(() -> new BlacklistToken(invalidId, LocalDateTime.now()))
                 .isInstanceOf(InvalidBlacklistTokenContentException.class)
@@ -62,7 +62,7 @@ class BlacklistTokenTest {
     @Test
     void 블랙리스트_토큰_초기화_시_유효한_블랙리스트_등록_일자가_아니라면_초기화_할_수_없다() {
         // when & then
-        assertThatThrownBy(() -> new BlacklistToken("id", null))
+        assertThatThrownBy(() -> new BlacklistToken("user1@naver.com", null))
                 .isInstanceOf(InvalidBlacklistTokenContentException.class)
                 .hasMessage("유효한 등록 일자가 아닙니다.");
     }
