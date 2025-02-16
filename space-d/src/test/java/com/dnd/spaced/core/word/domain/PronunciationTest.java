2b9f9a4bf9411d430d91b691234dc83a5c94f726
apptie <appti@naver.com>
2025-02-16 23:49:25 +0900

    test: Pronunciation 테스트 리팩터링

--- space-d/src/test/java/com/dnd/spaced/core/word/domain/PronunciationTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/word/domain/PronunciationTest.java
@@ -45,10 +45,10 @@ class PronunciationTest {
         // given
         Pronunciation pronunciation = new Pronunciation("어써라이제이션", "한글 발음");
         Word word = Word.builder()
-                         .name("Authorization")
-                         .meaning("word meaning")
-                         .categoryName("개발")
-                         .build();
+                        .name("Authorization")
+                        .meaning("Authorization(권한 부여)은 인증된 사용자가 특정 리소스나 기능에 접근할 수 있는 권한이 있는지를 확인하고 제어하는 보안 메커니즘")
+                        .categoryName("개발")
+                        .build();
 
         // when
         pronunciation.initWord(word);
