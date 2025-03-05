efb008c1479844fff03776f6882e8d0f4508db89
apptie <appti@naver.com>
2025-03-06 02:00:39 +0900

    feat: 용어 랜덤 조회 조건 관리 도메인 추가

---
+++ space-d/src/main/java/com/dnd/spaced/core/word/domain/WordRandom.java
@@ -0,0 +1,33 @@
+package com.dnd.spaced.core.word.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@EqualsAndHashCode(callSuper = false, of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WordRandom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long wordId;
+
+    private Category category;
+
+    private int random;
+
+    public WordRandom(Long wordId, Category category,int random) {
+        this.wordId = wordId;
+        this.category = category;
+        this.random = random;
+    }
+}
