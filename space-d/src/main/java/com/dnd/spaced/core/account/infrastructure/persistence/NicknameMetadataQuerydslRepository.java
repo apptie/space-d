8ffd3571ee862c23655a0a15581289f8cc4c7953
apptie <appti@naver.com>
2025-03-09 14:50:36 +0900

    refactor: 회원 영속화 관련 기능 패키지 변경

--- space-d/src/main/java/com/dnd/spaced/core/account/infrastructure/persistence/NicknameMetadataQuerydslRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/account/infrastructure/persistence/NicknameMetadataQuerydslRepository.java
@@ -1,4 +1,4 @@
-package com.dnd.spaced.core.account.infrastructure.repository;
+package com.dnd.spaced.core.account.infrastructure.persistence;
 
 import static com.dnd.spaced.core.account.domain.QNicknameMetadata.nicknameMetadata;
 
--- space-d/src/test/java/com/dnd/spaced/core/account/infrastructure/persistence/AccountCrudRepositoryTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/infrastructure/persistence/AccountCrudRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.dnd.spaced.core.account.infrastructure.repository;
+package com.dnd.spaced.core.account.infrastructure.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
