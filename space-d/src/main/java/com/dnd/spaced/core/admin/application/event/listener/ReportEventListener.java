7281aab926b6812cfa678029a4a7af2007b126b2
apptie <appti@naver.com>
2025-03-09 18:28:21 +0900

    refactor: 신고 후처리 이벤트리스너 메서드 가독성 개선

--- space-d/src/main/java/com/dnd/spaced/core/admin/application/event/listener/ReportEventListener.java
+++ space-d/src/main/java/com/dnd/spaced/core/admin/application/event/listener/ReportEventListener.java
@@ -19,10 +19,18 @@ public class ReportEventListener {
     @EventListener
     @Transactional
     public void processReport(ProcessedReportEvent event) {
-        Comment comment = commentRepository.findBy(event.commentId())
-                                           .orElseThrow(() -> new CommentNotFoundException("지정한 댓글을 찾을 수 없습니다."));
+        Comment comment = findComment(event);
         ReportStatus reportStatus = event.reportStatus();
 
+        processCommentBy(reportStatus, comment);
+    }
+
+    private Comment findComment(ProcessedReportEvent event) {
+        return commentRepository.findBy(event.commentId())
+                                .orElseThrow(() -> new CommentNotFoundException("지정한 댓글을 찾을 수 없습니다."));
+    }
+
+    private void processCommentBy(ReportStatus reportStatus, Comment comment) {
         if (reportStatus.isProcess()) {
             comment.delete();
             return;
