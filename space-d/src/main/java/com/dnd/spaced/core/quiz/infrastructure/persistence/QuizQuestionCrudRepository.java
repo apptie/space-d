9358f64a20a43720f62962f931baca4ef42ec0ea
apptie <appti@naver.com>
2025-03-10 23:17:10 +0900

    refactor: 퀴즈 영속화 관련 사용하지 않는 메서드 삭제

--- space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/QuizQuestionCrudRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/QuizQuestionCrudRepository.java
@@ -1,10 +1,7 @@
 package com.dnd.spaced.core.quiz.infrastructure.persistence;
 
 import com.dnd.spaced.core.quiz.domain.QuizQuestion;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface QuizQuestionCrudRepository extends CrudRepository<QuizQuestion, Long> {
-
-    List<QuizQuestion> findAllByQuizId(Long quizId);
+interface QuizQuestionCrudRepository extends CrudRepository<QuizQuestion, Long> {
 }
--- space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/TodayQuizGradedAnswerCrudRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/TodayQuizGradedAnswerCrudRepository.java
@@ -1,10 +1,7 @@
 package com.dnd.spaced.core.quiz.infrastructure.persistence;
 
 import com.dnd.spaced.core.quiz.domain.TodayQuizGradedAnswer;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 interface TodayQuizGradedAnswerCrudRepository extends CrudRepository<TodayQuizGradedAnswer, Long> {
-
-    List<TodayQuizGradedAnswer> findAllByAccountId(Long accountId);
 }
