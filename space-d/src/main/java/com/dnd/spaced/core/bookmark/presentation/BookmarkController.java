75429b680d72f36f46858e0f094aa217f29b60ff
apptie <appti@naver.com>
2025-03-10 22:21:19 +0900

    refactor: 북마크 API 핸들러 메서드 네이밍 변경

--- space-d/src/main/java/com/dnd/spaced/core/bookmark/presentation/BookmarkController.java
+++ space-d/src/main/java/com/dnd/spaced/core/bookmark/presentation/BookmarkController.java
@@ -28,7 +28,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public ResponseEntity<BookmarkCollectionResponse> findAllBy(
+    public ResponseEntity<BookmarkCollectionResponse> readBookmarks(
             @AuthAccount AuthAccountInfo accountInfo,
             ReadAllBookmarkRequest request,
             @BookmarkPageable Pageable pageable
@@ -39,7 +39,7 @@ public class BookmarkController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> create(
+    public ResponseEntity<Void> createBookmark(
             @AuthAccount AuthAccountInfo accountInfo,
             @RequestBody @Valid CreateBookmarkRequest request
     ) {
@@ -49,7 +49,10 @@ public class BookmarkController {
     }
 
     @DeleteMapping("{bookmarkId}")
-    public ResponseEntity<Void> delete(@AuthAccount AuthAccountInfo accountInfo, @PathVariable Long bookmarkId) {
+    public ResponseEntity<Void> deleteBookmark(
+            @AuthAccount AuthAccountInfo accountInfo,
+            @PathVariable Long bookmarkId
+    ) {
         bookmarkService.deleteBookmark(accountInfo.id(), bookmarkId);
 
         return ResponseEntityConst.NO_CONTENT;
