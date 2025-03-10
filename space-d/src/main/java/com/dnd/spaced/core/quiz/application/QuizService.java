b82d162077709c45e108d7c2535342555cbe8b2d
apptie <appti@naver.com>
2025-03-10 22:59:15 +0900

    refactor: 퀴즈 서비스 메서드 분리

--- space-d/src/main/java/com/dnd/spaced/core/quiz/application/QuizService.java
+++ space-d/src/main/java/com/dnd/spaced/core/quiz/application/QuizService.java
@@ -67,20 +67,19 @@ public class QuizService {
         Quiz quiz = createQuiz(accountId, quizCategory);
         Quiz savedQuiz = quizRepository.save(quiz);
 
-        eventPublisher.publishEvent(new AddedQuizQuestionEvent());
+        publishAddedQuizQuestionEvent();
         return savedQuiz.getId();
     }
 
     @Transactional
     public void grade(Long accountId, Long quizId, GradeQuizRequest request) {
-        Quiz quiz = quizRepository.findBy(quizId)
-                                  .orElseThrow(() -> new QuizNotFoundException("지정한 id의 퀴즈를 찾지 못했습니다."));
+        Quiz quiz = findQuiz(quizId);
         List<GradedAnswer> gradedAnswers = quiz.grade(accountId, request.answers());
 
         gradedAnswerRepository.saveAll(gradedAnswers);
         long correctCount = gradedAnswers.stream()
-                                  .filter(GradedAnswer::isCorrect)
-                                  .count();
+                                         .filter(GradedAnswer::isCorrect)
+                                         .count();
         eventPublisher.publishEvent(new GradedQuizEvent(accountId, correctCount));
     }
 
@@ -105,10 +104,18 @@ public class QuizService {
     }
 
     public QuizResponse findQuizBy(Long id) {
-        Quiz quiz = quizRepository.findBy(id)
-                                  .orElseThrow(() -> new QuizNotFoundException("지정한 id의 퀴즈를 찾지 못했습니다."));
+        Quiz quiz = findQuiz(id);
 
         return QuizApplicationMapper.toDto(quiz);
+    }
+
+    private Quiz findQuiz(Long id) {
+        return quizRepository.findBy(id)
+                             .orElseThrow(() -> new QuizNotFoundException("지정한 id의 퀴즈를 찾지 못했습니다."));
+    }
+
+    private void publishAddedQuizQuestionEvent() {
+        eventPublisher.publishEvent(new AddedQuizQuestionEvent());
     }
 
     private void validateQuizCreation(QuizCategory quizCategory) {
