e3cb0c966b395a81815e0f8fad0ec2d499f78e53
apptie <appti@naver.com>
2025-03-16 14:12:33 +0900

    fix: 용어 조회 시 발생하는 이벤트 리스너를 비동기로 변환

--- space-d/src/main/java/com/dnd/spaced/core/word/application/event/listener/WordViewCounterEventListener.java
+++ space-d/src/main/java/com/dnd/spaced/core/word/application/event/listener/WordViewCounterEventListener.java
@@ -4,23 +4,32 @@ import com.dnd.spaced.core.word.application.event.dto.WordViewCountIncrementEven
 import com.dnd.spaced.core.word.domain.repository.PopularWordRepository;
 import com.dnd.spaced.core.word.domain.repository.WordRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
-public class WordViewCountCalculateEventListener {
+public class WordViewCounterEventListener {
+
+    private static final String REQUEST_ID = "request_id";
 
     private final WordRepository wordRepository;
     private final PopularWordRepository popularWordRepository;
 
+    @Async("asyncWordViewCounterExecutor")
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
     public void listen(WordViewCountIncrementEvent event) {
         if (!popularWordRepository.existsBy(event.wordId(), event.localDateTime())) {
             wordRepository.updateViewCount(event.wordId());
+            String requestId = MDC.get(REQUEST_ID);
+            log.info("[{}] wordId : {}, localDateTime : {}", requestId, event.wordId(), event.localDateTime());
         }
     }
 }
