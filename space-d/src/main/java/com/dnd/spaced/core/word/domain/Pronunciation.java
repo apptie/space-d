a9f0838950cf753fb2b06ba8c63b27225e3204d5
apptie <appti@naver.com>
2025-03-16 13:03:52 +0900

    refactor: 용어 발음 타입 저장 방식 변경

--- space-d/src/main/java/com/dnd/spaced/core/word/domain/Pronunciation.java
+++ space-d/src/main/java/com/dnd/spaced/core/word/domain/Pronunciation.java
@@ -3,6 +3,8 @@ package com.dnd.spaced.core.word.domain;
 import com.dnd.spaced.core.word.domain.exception.InvalidPronunciationContentException;
 import com.dnd.spaced.global.audit.BaseTimeEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,6 +33,8 @@ public class Pronunciation extends BaseTimeEntity {
     private Word word;
 
     private String content;
+
+    @Enumerated(EnumType.STRING)
     private PronunciationType type;
 
     public Pronunciation(String content, String typeName) {
