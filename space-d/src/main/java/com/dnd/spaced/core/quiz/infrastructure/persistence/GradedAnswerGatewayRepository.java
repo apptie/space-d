8628869acaed0338d6faff75905d6589ad253ca9
apptie <appti@naver.com>
2025-03-20 08:25:44 +0900

    fix: 퀴즈 정답 제출 bulk insert 로직 변경

--- space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/GradedAnswerGatewayRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/GradedAnswerGatewayRepository.java
@@ -6,21 +6,50 @@ import com.dnd.spaced.core.quiz.domain.GradedAnswer;
 import com.dnd.spaced.core.quiz.domain.repository.GradedAnswerRepository;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class GradedAnswerGatewayRepository implements GradedAnswerRepository {
 
+    private final Clock clock;
+    private final JdbcTemplate jdbcTemplate;
     private final JPAQueryFactory queryFactory;
-    private final GradedAnswerCrudRepository gradedAnswerCrudRepository;
 
     @Override
     public void saveAll(List<GradedAnswer> gradedAnswers) {
-        gradedAnswerCrudRepository.saveAll(gradedAnswers);
+        String sql = """
+                INSERT INTO graded_answer(created_at, account_id, quiz_id, quiz_question_id, selected_word_id)
+                VALUES(?, ?, ?, ?, ?)
+                """;
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                GradedAnswer gradedAnswer = gradedAnswers.get(i);
+
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now(clock)));
+                ps.setLong(2, gradedAnswer.getAccountId());
+                ps.setLong(3, gradedAnswer.getQuizId());
+                ps.setLong(4, gradedAnswer.getQuizQuestion().getId());
+                ps.setLong(5, gradedAnswer.getSelectedWordId());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return gradedAnswers.size();
+            }
+        });
     }
 
     @Override
