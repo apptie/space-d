274c8b722f9a5eae10536d88bdb5a498f95233fb
apptie <appti@naver.com>
2025-03-23 11:38:33 +0900

    fix: 회원 서비스 클래스 레벨 트랜잭션 제거

--- space-d/src/main/java/com/dnd/spaced/core/account/application/AccountService.java
+++ space-d/src/main/java/com/dnd/spaced/core/account/application/AccountService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AccountService {
 
