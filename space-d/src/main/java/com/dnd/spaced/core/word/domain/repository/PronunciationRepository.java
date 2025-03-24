3af35722935c1e7d3e421966416f1c14d9228cb8
apptie <appti@naver.com>
2025-03-24 20:06:08 +0900

    feat: 용어 발음 bulk insert 기능 추가

--- space-d/src/main/java/com/dnd/spaced/core/word/domain/repository/PronunciationRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/word/domain/repository/PronunciationRepository.java
@@ -1,6 +1,11 @@
 package com.dnd.spaced.core.word.domain.repository;
 
+import com.dnd.spaced.core.word.domain.Pronunciation;
+import java.util.List;
+
 public interface PronunciationRepository {
+
+    void saveAll(List<Pronunciation> pronunciations);
 
     long countBy(Long wordId);
 
--- space-d/src/main/java/com/dnd/spaced/core/word/infrastructure/persistence/PronunciationGatewayRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/word/infrastructure/persistence/PronunciationGatewayRepository.java
@@ -2,17 +2,57 @@ package com.dnd.spaced.core.word.infrastructure.persistence;
 
 import static com.dnd.spaced.core.word.domain.QPronunciation.pronunciation;
 
+import com.dnd.spaced.core.word.domain.Pronunciation;
 import com.dnd.spaced.core.word.domain.repository.PronunciationRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class PronunciationGatewayRepository implements PronunciationRepository {
 
+    private static final int DEFAULT_BATCH_INSERT_SIZE = 20;
+
+    private final Clock clock;
+    private final JdbcTemplate jdbcTemplate;
     private final JPAQueryFactory queryFactory;
     private final PronunciationCrudRepository pronunciationCrudRepository;
+
+    @Override
+    public void saveAll(List<Pronunciation> pronunciations) {
+        String sql = """
+                INSERT INTO pronunciations(created_at, updated_at, content, type, word_id)
+                VALUES(?, ?, ?, ?, ?);
+                """;
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                Pronunciation pronunciation = pronunciations.get(i);
+
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now(clock)));
+                ps.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now(clock)));
+                ps.setString(3, pronunciation.getContent());
+                ps.setString(4, pronunciation.getType().name());
+                ps.setLong(5, pronunciation.getWord().getId());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return Math.min(DEFAULT_BATCH_INSERT_SIZE, pronunciations.size());
+            }
+        });
+    }
 
     @Override
     public long countBy(Long wordId) {
