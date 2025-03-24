9de4e70053e3580512799b36eda252c0983a0247
apptie <appti@naver.com>
2025-03-24 17:04:05 +0900

    test: 관리자 신고 서비스 테스트 초기화 방식을 sql 방식으로 변경

---
+++ space-d/src/test/resources/sql/admin/report/comment.sql
@@ -0,0 +1,2 @@
+INSERT INTO comments(id, created_at, updated_at, content, is_deleted, like_count, word_id, writer_id)
+VALUES(1, now(), now(), '친구초대 특별이벤트 링크 : http://abcdef.com', false, 0, 1, 1);
---
+++ space-d/src/test/resources/sql/admin/report/report.sql
@@ -0,0 +1,2 @@
+INSERT INTO reports(id, created_at, comment_id, report_status, reporter_id, report_reason)
+VALUES(1, now(), 1, 'PENDING', 2, 'SPAM');
