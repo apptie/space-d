58a7e2c02fc6b45750c61d5e5ba644a5c2beaf03
apptie <appti@naver.com>
2025-03-24 20:06:22 +0900

    feat: 용어 예문 bulk insert 기능 추가

--- space-d/src/main/java/com/dnd/spaced/core/word/domain/repository/WordExampleRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/word/domain/repository/WordExampleRepository.java
@@ -1,6 +1,11 @@
 package com.dnd.spaced.core.word.domain.repository;
 
+import com.dnd.spaced.core.word.domain.WordExample;
+import java.util.List;
+
 public interface WordExampleRepository {
+
+    void saveAll(List<WordExample> wordExamples);
 
     long countBy(Long wordId);
 
--- space-d/src/main/java/com/dnd/spaced/core/word/infrastructure/persistence/WordExampleGatewayRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/word/infrastructure/persistence/WordExampleGatewayRepository.java
@@ -2,15 +2,28 @@ package com.dnd.spaced.core.word.infrastructure.persistence;
 
 import static com.dnd.spaced.core.word.domain.QWordExample.wordExample;
 
+import com.dnd.spaced.core.word.domain.WordExample;
 import com.dnd.spaced.core.word.domain.repository.WordExampleRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class WordExampleGatewayRepository implements WordExampleRepository {
 
+    private static final int DEFAULT_BATCH_INSERT_SIZE = 10;
+
+    private final Clock clock;
+    private final JdbcTemplate jdbcTemplate;
     private final JPAQueryFactory queryFactory;
     private final WordExampleCrudRepository wordExampleCrudRepository;
 
@@ -32,5 +45,31 @@ public class WordExampleGatewayRepository implements WordExampleRepository {
     @Override
     public void deleteBy(Long wordExampleId) {
         wordExampleCrudRepository.deleteById(wordExampleId);
+    }
+
+    @Override
+    public void saveAll(List<WordExample> wordExamples) {
+        String sql = """
+                INSERT INTO word_examples(created_at, updated_at, example, word_id)
+                VALUES(?, ?, ?, ?)
+                """;
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                WordExample wordExample = wordExamples.get(i);
+
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now(clock)));
+                ps.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now(clock)));
+                ps.setString(3, wordExample.getExample());
+                ps.setLong(4, wordExample.getWord().getId());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return Math.min(DEFAULT_BATCH_INSERT_SIZE, wordExamples.size());
+            }
+        });
     }
 }
