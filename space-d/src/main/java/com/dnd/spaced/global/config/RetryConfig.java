a532f3d23d08440a476c77f8ea783a58a1e71361
apptie <appti@naver.com>
2025-03-25 14:18:55 +0900

    chore: spring retry 설정 추가

---
+++ space-d/src/main/java/com/dnd/spaced/global/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.dnd.spaced.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}
