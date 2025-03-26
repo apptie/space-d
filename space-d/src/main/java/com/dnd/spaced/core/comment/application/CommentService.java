baa24927934aa005a29935dfde4a1ff992ece783
apptie <appti@naver.com>
2025-03-26 12:48:15 +0900

    fix: 댓글 서비스 클래스 레벨 트랜잭션 제거

--- space-d/src/main/java/com/dnd/spaced/core/comment/application/CommentService.java
+++ space-d/src/main/java/com/dnd/spaced/core/comment/application/CommentService.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CommentService {
 
