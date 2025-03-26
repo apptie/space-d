f39c3f7c372486a4e9a1842d8e63248a4eaf3334
apptie <appti@naver.com>
2025-03-26 22:23:15 +0900

    test: 로그인 서비스 테스트 트랜잭션 기반에서 sql 기반으로 변경

--- space-d/src/test/java/com/dnd/spaced/core/auth/application/LoginServiceTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/auth/application/LoginServiceTest.java
@@ -4,9 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import com.dnd.spaced.config.clean.annotation.CleanUpDatabase;
-import com.dnd.spaced.core.account.domain.NicknameMetadata;
-import com.dnd.spaced.core.account.domain.repository.NicknameMetadataRepository;
 import com.dnd.spaced.core.auth.application.dto.response.LoggedInAccountInfoDto;
 import com.dnd.spaced.core.auth.application.exception.NicknameMetadataNotFoundException;
 import com.dnd.spaced.core.skill.application.event.dto.InitializedAccountEvent;
@@ -18,10 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.jdbc.Sql;
 
-@Transactional
-@CleanUpDatabase
 @RecordApplicationEvents
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -34,14 +29,9 @@ class LoginServiceTest {
     @Autowired
     ApplicationEvents events;
 
-    @Autowired
-    NicknameMetadataRepository nicknameMetadataRepository;
-
     @Test
+    @Sql(scripts = {"classpath:sql/cleanup.sql", "classpath:sql/auth/nickname_metadata.sql"})
     void 회원가입하지_않은_회원이_로그인하면_회원_가입과_로그인_절차를_진행한다() {
-        // given
-        nicknameMetadataRepository.save(NicknameMetadata.from("재빠른지구"));
-
         // when
         LoggedInAccountInfoDto actual = loginService.login("kakao", "12345");
 
@@ -55,11 +45,13 @@ class LoginServiceTest {
     }
 
     @Test
+    @Sql(scripts = {
+            "classpath:sql/cleanup.sql",
+            "classpath:sql/auth/nickname_metadata.sql",
+            "classpath:sql/auth/account.sql"
+    })
     void 회원가입한_회원이_로그인하면_로그인_절차를_진행한다() {
         // given
-        nicknameMetadataRepository.save(NicknameMetadata.from("재빠른지구"));
-        loginService.login("kakao", "12345");
-
         // when
         LoggedInAccountInfoDto actual = loginService.login("kakao", "12345");
 
@@ -68,32 +60,12 @@ class LoginServiceTest {
                 () -> assertThat(actual.id()).isPositive(),
                 () -> assertThat(actual.roleName()).isEqualTo("ROLE_USER"),
                 () -> assertThat(actual.isSignUp()).isFalse(),
-                () -> assertThat(events.stream(InitializedAccountEvent.class).count()).isOne()
+                () -> assertThat(events.stream(InitializedAccountEvent.class).count()).isZero()
         );
     }
 
     @Test
-    void 회원가입하지_않은_회원이_로그인하면서_회원_가입_절차에서_기존_닉네임과_동일한_닉네임을_부여받으면_닉네임_메타데이터를_갱신하고_로그인_절차를_진행한다() {
-        // given
-        nicknameMetadataRepository.save(NicknameMetadata.from("재빠른지구"));
-        LoggedInAccountInfoDto loggedInAccountInfo1 = loginService.login("kakao", "12345");
-
-        // when
-        LoggedInAccountInfoDto loggedInAccountInfo2 = loginService.login("kakao", "54321");
-        NicknameMetadata nicknameMetadata = nicknameMetadataRepository.findBy("재빠른지구")
-                                                                      .get();
-
-        // then
-        assertAll(
-                () -> assertThat(loggedInAccountInfo2.id()).isNotEqualTo(loggedInAccountInfo1.id()),
-                () -> assertThat(loggedInAccountInfo2.roleName()).isEqualTo("ROLE_USER"),
-                () -> assertThat(loggedInAccountInfo2.isSignUp()).isTrue(),
-                () -> assertThat(nicknameMetadata.getCount()).isEqualTo(2L),
-                () -> assertThat(events.stream(InitializedAccountEvent.class).count()).isEqualTo(2L)
-        );
-    }
-
-    @Test
+    @Sql("classpath:sql/cleanup.sql")
     void 닉네임_메타데이터가_정상적으로_초기화되지_않았다면_로그인을_할_수_없다() {
         // when & then
         assertThatThrownBy(() -> loginService.login("kakao", "54321"))
