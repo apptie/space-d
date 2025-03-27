0c18fd374eadca81808a19f5e87905b09e0cd000
apptie <appti@naver.com>
2025-03-28 00:15:08 +0900

    test: Mock이나 Spy 빈 생성 시 애플리케이션 컨텍스트를 유지해주는 테스트 리스너 추가

---
+++ space-d/src/test/java/com/dnd/spaced/config/annotation/SpyInContextBean.java
@@ -0,0 +1,15 @@
+package com.dnd.spaced.config.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SpyInContextBean {
+
+    Class<?>[] value();
+
+    String name() default "";
+}
