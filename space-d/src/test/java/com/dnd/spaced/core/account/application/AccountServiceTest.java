4dd9f811b3e0dbc5eae77160755bd4d4cf9720de
apptie <appti@naver.com>
2025-03-28 18:59:27 +0900

    test: 회원 서비스 테스트 방식 트랜잭션에서 sql 기반으로 변경

--- space-d/src/test/java/com/dnd/spaced/core/account/application/AccountServiceTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/account/application/AccountServiceTest.java
@@ -3,17 +3,13 @@ package com.dnd.spaced.core.account.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import com.dnd.spaced.config.clean.annotation.CleanUpDatabase;
 import com.dnd.spaced.core.account.application.dto.request.ChangeCareerInfoRequest;
 import com.dnd.spaced.core.account.application.dto.request.ChangeProfileInfoRequest;
 import com.dnd.spaced.core.account.application.dto.response.AccountResponse;
 import com.dnd.spaced.core.account.application.exception.ForbiddenAccountException;
-import com.dnd.spaced.core.account.application.helper.AccountServiceTestHelper;
-import com.dnd.spaced.core.account.domain.Account;
 import com.dnd.spaced.core.account.domain.enums.ProfileImageName;
-import com.dnd.spaced.core.account.domain.enums.RegistrationId;
-import com.dnd.spaced.core.account.domain.enums.Role;
 import com.dnd.spaced.core.account.domain.enums.exception.InvalidCompanyException;
 import com.dnd.spaced.core.account.domain.enums.exception.InvalidExperienceException;
 import com.dnd.spaced.core.account.domain.enums.exception.InvalidJobGroupException;
@@ -30,25 +26,21 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.jdbc.Sql;
 
-@Transactional
-@CleanUpDatabase
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class AccountServiceTest extends AccountServiceTestHelper {
+class AccountServiceTest {
 
     @Autowired
     AccountService accountService;
 
     @Test
+    @Sql("classpath:sql/account/account.sql")
     void 지정한_회원을_탈퇴_처리한다() {
-        // when
-        accountService.withdrawal(account.getId());
-
-        // then
-        assertThat(accountRepository.findBy(account.getId())).isEmpty();
+        // when & then
+        assertDoesNotThrow(() -> accountService.withdrawal(1L));
     }
 
     @Test
@@ -60,6 +52,7 @@ class AccountServiceTest extends AccountServiceTestHelper {
     }
 
     @Test
+    @Sql("classpath:sql/account/account.sql")
     void 회원_경력_정보를_변경한다() {
         // given
         ChangeCareerInfoRequest request = new ChangeCareerInfoRequest(
@@ -69,10 +62,10 @@ class AccountServiceTest extends AccountServiceTestHelper {
         );
 
         // when
-        accountService.changeCareerInfo(account.getId(), request);
+        accountService.changeCareerInfo(1L, request);
 
         // then
-        AccountResponse actual = accountService.readAccount(account.getId());
+        AccountResponse actual = accountService.readAccount(1L);
 
         assertAll(
                 () -> assertThat(actual.jobGroupName()).isEqualTo("개발자"),
@@ -82,7 +75,8 @@ class AccountServiceTest extends AccountServiceTestHelper {
     }
 
     @ParameterizedTest(name = "직군 이름이 {0}일 때 경력 정보를 변경할 수 없다.")
-    @NullAndEmptySource
+    @NullAndEmptySource // sql/cleanup.sql
+    @Sql("classpath:sql/account/account.sql")
     void 유효한_직군_이름이_아니라면_경력_정보를_변경할_수_없다(String invalidJobGroupName) {
         // given
         ChangeCareerInfoRequest request = new ChangeCareerInfoRequest(
@@ -92,13 +86,14 @@ class AccountServiceTest extends AccountServiceTestHelper {
         );
 
         // when & then
-        assertThatThrownBy(() -> accountService.changeCareerInfo(account.getId(), request))
+        assertThatThrownBy(() -> accountService.changeCareerInfo(1L, request))
                 .isInstanceOf(InvalidJobGroupException.class)
                 .hasMessageContaining("잘못된 직군 이름");
     }
 
     @ParameterizedTest(name = "회사명이 {0}일 때 경력 정보를 변경할 수 없다.")
     @NullAndEmptySource
+    @Sql("classpath:sql/account/account.sql")
     void 유효한_회사명이_아니라면_경력_정보를_변경할_수_없다(String invalidCompanyName) {
         // given
         ChangeCareerInfoRequest request = new ChangeCareerInfoRequest(
@@ -108,13 +103,14 @@ class AccountServiceTest extends AccountServiceTestHelper {
         );
 
         // when & then
-        assertThatThrownBy(() -> accountService.changeCareerInfo(account.getId(), request))
+        assertThatThrownBy(() -> accountService.changeCareerInfo(1L, request))
                 .isInstanceOf(InvalidCompanyException.class)
                 .hasMessageContaining("잘못된 회사 이름");
     }
 
     @ParameterizedTest(name = "경력이 {0}일 때 경력 정보를 변경할 수 없다.")
     @NullAndEmptySource
+    @Sql("classpath:sql/account/account.sql")
     void 유효한_경력이_아니라면_경력_정보를_변경할_수_없다(String invalidExperienceName) {
         // given
         ChangeCareerInfoRequest request = new ChangeCareerInfoRequest(
@@ -124,7 +120,7 @@ class AccountServiceTest extends AccountServiceTestHelper {
         );
 
         // when & then
-        assertThatThrownBy(() -> accountService.changeCareerInfo(account.getId(), request))
+        assertThatThrownBy(() -> accountService.changeCareerInfo(1L, request))
                 .isInstanceOf(InvalidExperienceException.class)
                 .hasMessageContaining("잘못된 경력");
     }
@@ -151,6 +147,7 @@ class AccountServiceTest extends AccountServiceTestHelper {
 
     @ParameterizedTest(name = "프로필 이미지를 유효한 프로필 이미지 경로인 {0}으로 변경한다")
     @MethodSource("changeProfileInfoTestWithProfileImageKoreanName")
+    @Sql("classpath:sql/account/account.sql")
     void 회원_프로필_정보를_변경한다(ProfileImageName profileImageName) {
         // given
         ChangeProfileInfoRequest request = new ChangeProfileInfoRequest(
@@ -159,10 +156,10 @@ class AccountServiceTest extends AccountServiceTestHelper {
         );
 
         // when
-        accountService.changeProfileInfo(account.getId(), request);
+        accountService.changeProfileInfo(1L, request);
 
         // then
-        AccountResponse actual = accountService.readAccount(account.getId());
+        AccountResponse actual = accountService.readAccount(1L);
 
         assertAll(
                 () -> assertThat(actual.nickname()).isEqualTo("행복한지구001"),
@@ -172,6 +169,7 @@ class AccountServiceTest extends AccountServiceTestHelper {
 
     @ParameterizedTest(name = "프로필 이미지가 {0}일 때 프로필 정보를 변경할 수 없다.")
     @NullAndEmptySource
+    @Sql("classpath:sql/account/account.sql")
     void 프로필_이미지_경로가_비어_있으면_프로필_정보를_변경할_수_없다(String invalidProfileImageKoreanName) {
         // given
         ChangeProfileInfoRequest request = new ChangeProfileInfoRequest(
@@ -180,7 +178,7 @@ class AccountServiceTest extends AccountServiceTestHelper {
         );
 
         // when & then
-        assertThatThrownBy(() -> accountService.changeProfileInfo(account.getId(), request))
+        assertThatThrownBy(() -> accountService.changeProfileInfo(1L, request))
                 .isInstanceOf(InvalidProfileImageNameException.class)
                 .hasMessageContaining("잘못된 프로필 이미지 이름");
     }
@@ -200,31 +198,16 @@ class AccountServiceTest extends AccountServiceTestHelper {
     }
 
     @Test
+    @Sql("classpath:sql/account/account.sql")
     void 회원_정보를_조회한다() {
-        // given
-        Account account = Account.builder()
-                                 .registrationId(RegistrationId.KAKAO)
-                                 .socialIdentifier("12345")
-                                 .nickname("재빠른지구001")
-                                 .profileImage("earth.png")
-                                 .role(Role.ROLE_USER)
-                                 .build();
-
-        account.changeCareerInfo(
-                "개발자",
-                "비공개",
-                "1~2년 차"
-        );
-        accountRepository.save(account);
-
         // when
-        AccountResponse actual = accountService.readAccount(account.getId());
+        AccountResponse actual = accountService.readAccount(1L);
 
         // then
         assertAll(
-                () -> assertThat(actual.jobGroupName()).isEqualTo("개발자"),
-                () -> assertThat(actual.companyName()).isEqualTo("비공개"),
-                () -> assertThat(actual.experienceName()).isEqualTo("1~2년 차"),
+                () -> assertThat(actual.jobGroupName()).isEqualTo("기타"),
+                () -> assertThat(actual.companyName()).isEqualTo("스타트업"),
+                () -> assertThat(actual.experienceName()).isEqualTo("1년 차 미만"),
                 () -> assertThat(actual.nickname()).isEqualTo("재빠른지구001"),
                 () -> assertThat(actual.profileImage()).isEqualTo("earth.png")
         );
