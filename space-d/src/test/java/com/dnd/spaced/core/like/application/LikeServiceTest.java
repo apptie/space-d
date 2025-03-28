d7d3adee6dc8f893cf4341bcb6872793d6fd1505
apptie <appti@naver.com>
2025-03-28 21:24:33 +0900

    test: 좋아요 서비스 테스트 방식 트랜잭션에서 sql 기반으로 변경

--- space-d/src/test/java/com/dnd/spaced/core/like/application/LikeServiceTest.java
+++ space-d/src/test/java/com/dnd/spaced/core/like/application/LikeServiceTest.java
@@ -2,16 +2,11 @@ package com.dnd.spaced.core.like.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
-import com.dnd.spaced.config.clean.annotation.CleanUpDatabase;
 import com.dnd.spaced.core.comment.application.event.dto.LikedEvent;
+import com.dnd.spaced.core.comment.application.event.dto.UnlikedEvent;
 import com.dnd.spaced.core.like.application.exception.AssociationCommentNotFoundException;
 import com.dnd.spaced.core.like.application.exception.ForbiddenLikeException;
-import com.dnd.spaced.core.like.application.helper.WithAccountAndCommentTestHelper;
-import com.dnd.spaced.core.like.domain.Like;
-import com.dnd.spaced.core.like.domain.repository.LikeRepository;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -20,68 +15,61 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.jdbc.Sql;
 
-@Transactional
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @SuppressWarnings("NonAsciiCharacters")
 @RecordApplicationEvents
-@CleanUpDatabase
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class LikeServiceTest extends WithAccountAndCommentTestHelper {
+class LikeServiceTest {
 
     @Autowired
     LikeService likeService;
 
     @Autowired
-    LikeRepository likeRepository;
-
-    @Autowired
     ApplicationEvents events;
 
     @Test
+    @Sql(value = {
+            "classpath:sql/like/account.sql",
+            "classpath:sql/like/comment.sql"
+    })
     void 좋아요를_추가한다() {
         // when
-        likeService.processLike(account.getId(), comment.getId());
+        likeService.processLike(1L, 1L);
 
         // then
-        Optional<Like> actual = likeRepository.findBy(account.getId(), comment.getId());
-
-        assertAll(
-                () -> assertThat(actual).isPresent(),
-                () -> assertThat(events.stream(LikedEvent.class).count()).isOne()
-        );
+        assertThat(events.stream(LikedEvent.class).count()).isOne();
     }
 
     @Test
+    @Sql(value = {
+            "classpath:sql/like/account.sql",
+            "classpath:sql/like/comment.sql",
+            "classpath:sql/like/like.sql"
+    })
     void 좋아요를_취소한다() {
-        // given
-        likeService.processLike(account.getId(), comment.getId());
-
         // when
-        likeService.processLike(account.getId(), comment.getId());
+        likeService.processLike(1L, 1L);
 
         // then
-        Optional<Like> actual = likeRepository.findBy(account.getId(), comment.getId());
-
-        assertAll(
-                () -> assertThat(actual).isEmpty(),
-                () -> assertThat(events.stream(LikedEvent.class).count()).isOne()
-        );
+        assertThat(events.stream(UnlikedEvent.class).count()).isOne();
     }
 
     @Test
+    @Sql("classpath:sql/like/account.sql")
     void 존재하지_않는_댓글_ID로_좋아요를_할_수_없다() {
         // when & then
-        assertThatThrownBy(() -> likeService.processLike(account.getId(), -1L))
+                assertThatThrownBy(() -> likeService.processLike(1L, -999L))
                 .isInstanceOf(AssociationCommentNotFoundException.class)
                 .hasMessage("좋아요 대상인 댓글을 찾을 수 없습니다.");
     }
 
     @Test
+    @Sql("classpath:sql/like/comment.sql")
     void 없거나_이미_탈퇴한_회원_ID로_좋아요를_할_수_없다() {
         // when & then
-        assertThatThrownBy(() -> likeService.processLike(-999L, comment.getId()))
+        assertThatThrownBy(() -> likeService.processLike(-999L, 1L))
                 .isInstanceOf(ForbiddenLikeException.class)
                 .hasMessage("지정한 ID에 대한 회원을 찾지 못했습니다.");
     }
