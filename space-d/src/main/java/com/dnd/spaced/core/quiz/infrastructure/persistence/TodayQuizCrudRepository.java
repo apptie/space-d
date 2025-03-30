90f134b866abfc20913e255a35129b8c61ff9ad8
apptie <appti@naver.com>
2025-03-30 12:26:56 +0900

    refactor: 퀴즈 관련 JPA 리포지토리 접근 제어자를 package-private으로 변경

--- space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/TodayQuizCrudRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/TodayQuizCrudRepository.java
@@ -3,5 +3,5 @@ package com.dnd.spaced.core.quiz.infrastructure.persistence;
 import com.dnd.spaced.core.quiz.domain.TodayQuiz;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodayQuizCrudRepository extends CrudRepository<TodayQuiz, Long> {
+interface TodayQuizCrudRepository extends CrudRepository<TodayQuiz, Long> {
 }
--- space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/TodayQuizGradedAnswerCrudRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/quiz/infrastructure/persistence/TodayQuizGradedAnswerCrudRepository.java
@@ -3,5 +3,5 @@ package com.dnd.spaced.core.quiz.infrastructure.persistence;
 import com.dnd.spaced.core.quiz.domain.TodayQuizGradedAnswer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodayQuizGradedAnswerCrudRepository extends CrudRepository<TodayQuizGradedAnswer, Long> {
+interface TodayQuizGradedAnswerCrudRepository extends CrudRepository<TodayQuizGradedAnswer, Long> {
 }
