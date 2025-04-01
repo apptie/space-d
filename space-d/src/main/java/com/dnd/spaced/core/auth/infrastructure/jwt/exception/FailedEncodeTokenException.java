c726f537fb4018a7c3a39e1ac265685a4e2cab88
apptie <appti@naver.com>
2025-04-01 14:21:01 +0900

    docs: 문서 최신화

--- space-d/src/main/java/com/dnd/spaced/core/auth/infrastructure/jwt/exception/FailedEncodeTokenException.java
+++ space-d/src/main/java/com/dnd/spaced/core/auth/infrastructure/jwt/exception/FailedEncodeTokenException.java
@@ -1,9 +1,9 @@
 package com.dnd.spaced.core.auth.infrastructure.jwt.exception;
 
-import com.dnd.spaced.global.exception.base.AuthClientException;
+import com.dnd.spaced.global.exception.base.AuthServerException;
 import com.dnd.spaced.global.exception.code.AuthErrorCode;
 
-public class FailedEncodeTokenException extends AuthClientException {
+public class FailedEncodeTokenException extends AuthServerException {
 
     public FailedEncodeTokenException(String message, Throwable e) {
         super(AuthErrorCode.FAILED_ENCODE_TOKEN_EXCEPTION, message, e);
