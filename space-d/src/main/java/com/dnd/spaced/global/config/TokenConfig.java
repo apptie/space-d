dae5f0b0f7c042df6b5f888c8fb69380499b6694
apptie <appti@naver.com>
2025-04-01 14:42:35 +0900

    fix: 이전 암호화 방식 제거

--- space-d/src/main/java/com/dnd/spaced/global/config/TokenConfig.java
+++ space-d/src/main/java/com/dnd/spaced/global/config/TokenConfig.java
@@ -6,10 +6,6 @@ import com.dnd.spaced.core.auth.infrastructure.jwt.JwsSignerFinder;
 import com.dnd.spaced.core.auth.infrastructure.jwt.JwsVerifierFinder;
 import com.dnd.spaced.core.auth.infrastructure.jwt.JwtDecoder;
 import com.dnd.spaced.core.auth.infrastructure.jwt.JwtEncoder;
-import com.dnd.spaced.global.auth.encryptor.Encryptor;
-import com.dnd.spaced.global.auth.encryptor.CbcEncryptor;
-import com.dnd.spaced.global.auth.encryptor.DelegatingEncryptor;
-import com.dnd.spaced.global.auth.encryptor.GcmEncryptor;
 import com.dnd.spaced.global.config.properties.AesProperties;
 import com.dnd.spaced.global.config.properties.TokenProperties;
 import com.nimbusds.jose.JOSEException;
@@ -23,7 +19,6 @@ import com.nimbusds.jose.crypto.MACVerifier;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.time.Clock;
-import java.util.Map;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -35,23 +30,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(AesProperties.class)
-public class EncryptorConfig {
+public class TokenConfig {
 
     private final Clock clock;
-    private final AesProperties aesProperties;
     private final TokenProperties tokenProperties;
-
-    @Bean
-    public Encryptor aesEncryptor() {
-        GcmEncryptor gcmAesEncryptor = new GcmEncryptor(aesProperties.secretKey(), aesProperties.salt());
-        CbcEncryptor cbcAesEncryptor = new CbcEncryptor(aesProperties.secretKey(), aesProperties.salt());
-
-        return new DelegatingEncryptor(
-                "gcm",
-                Map.of("gcm", gcmAesEncryptor, "cbc", cbcAesEncryptor),
-                cbcAesEncryptor
-        );
-    }
 
     @Bean
     public TokenEncoder tokenEncoder(DirectEncrypter directEncrypter, JwsSignerFinder jwsSignerFinder) {
