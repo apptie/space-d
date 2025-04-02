d2694527df06579b356377447a481c18e6ef3ef6
apptie <appti@naver.com>
2025-04-02 22:38:02 +0900

    test: 컨트롤러 슬라이스 테스트 빈 주입 로직 변경

--- space-d/src/test/java/com/dnd/spaced/config/context/RestControllerTestInitializer.java
+++ space-d/src/test/java/com/dnd/spaced/config/context/RestControllerTestInitializer.java
@@ -1,5 +1,7 @@
 package com.dnd.spaced.config.context;
 
+import static org.mockito.Mockito.*;
+
 import com.dnd.spaced.config.context.exception.TestContextClassNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -14,7 +16,6 @@ import java.util.Set;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -28,135 +29,93 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class RestControllerTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
+    private static final String BASE_PACKAGE = "com.dnd.spaced";
+
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         BeanDefinitionRegistry registry = (BeanDefinitionRegistry) applicationContext;
-
-        registerConfigurationBean(applicationContext);
-        registerControllerSliceTestBean(registry);
+        registerConfigurationBeans(registry);
+        registerControllerSliceTestBeans(registry);
     }
 
-    private void registerConfigurationBean(ConfigurableApplicationContext applicationContext) {
-        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
-
-        beanFactory.registerSingleton("objectMapper", objectMapper());
-        beanFactory.registerSingleton("provider", new ManualRestDocumentation());
+    private void registerConfigurationBeans(BeanDefinitionRegistry registry) {
+        registerBeanDefinition(
+                registry,
+                "objectMapper",
+                ObjectMapper.class,
+                createCustomObjectMapper()
+        );
+        registerBeanDefinition(
+                registry,
+                "provider",
+                ManualRestDocumentation.class,
+                new ManualRestDocumentation()
+        );
     }
 
-    private ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper createCustomObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
 
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        objectMapper.setDateFormat(dateFormat);
-
-        return objectMapper;
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        return mapper;
     }
 
-    private void registerControllerSliceTestBean(BeanDefinitionRegistry registry) {
-        Set<Class<?>> controllerClasses = findRestControllerClasses();
-        Map<Class<?>, Object> mockServices = new HashMap<>();
+    private void registerControllerSliceTestBeans(BeanDefinitionRegistry registry) {
+        Map<Class<?>, Object> serviceMocks = new HashMap<>();
 
-        for (Class<?> controllerClass : controllerClasses) {
-            Map<String, Class<?>> dependencies = analyzeControllerDependencies(controllerClass);
+        for (Class<?> controllerClass : findRestControllerClasses()) {
+            Map<String, Class<?>> dependencies = analyzeDependencies(controllerClass);
 
-            processMockServiceBean(registry, dependencies, mockServices);
-            registerControllerBean(registry, controllerClass, dependencies);
+            registerDependencyMocks(registry, dependencies, serviceMocks);
+            registerControllerDefinition(registry, controllerClass, dependencies);
         }
     }
 
-    private void processMockServiceBean(
-            BeanDefinitionRegistry registry,
-            Map<String, Class<?>> dependencies,
-            Map<Class<?>, Object> mockServices
-    ) {
-        for (Map.Entry<String, Class<?>> entry : dependencies.entrySet()) {
-            Class<?> serviceClass = entry.getValue();
-            Object mockService = mockServices.computeIfAbsent(serviceClass, Mockito::mock);
-            String serviceBeanName = getBeanName(serviceClass);
-
-            registerMockServiceBean(registry, serviceBeanName, serviceClass, mockService);
-        }
-    }
-
-    private Set<Class<?>> findRestControllerClasses() {
-        Set<Class<?>> results = new HashSet<>();
-
-        ClassPathScanningCandidateComponentProvider scanner =
-                new ClassPathScanningCandidateComponentProvider(false);
-        scanner.addIncludeFilter(new AnnotationTypeFilter(RestController.class));
-
-        final String basePackage = "com.dnd.spaced";
-
-        for (BeanDefinition beanDefinition : scanner.findCandidateComponents(basePackage)) {
-            try {
-                results.add(Class.forName(beanDefinition.getBeanClassName()));
-            } catch (ClassNotFoundException e) {
-                throw new TestContextClassNotFoundException(e);
-            }
-        }
-
-        return results;
-    }
-
-    private Map<String, Class<?>> analyzeControllerDependencies(Class<?> controllerClass) {
+    private Map<String, Class<?>> analyzeDependencies(Class<?> controllerClass) {
         Map<String, Class<?>> dependencies = new HashMap<>();
         Constructor<?> constructor = controllerClass.getDeclaredConstructors()[0];
 
         for (Parameter param : constructor.getParameters()) {
-            Class<?> paramType = param.getType();
-
-            dependencies.put(param.getName(), paramType);
+            dependencies.put(param.getName(), param.getType());
         }
-
         return dependencies;
     }
 
-    private void registerMockServiceBean(
+    private void registerDependencyMocks(
             BeanDefinitionRegistry registry,
-            String beanName,
-            Class<?> serviceClass,
-            Object mockService
+            Map<String, Class<?>> dependencies,
+            Map<Class<?>, Object> serviceMocks
     ) {
-        GenericBeanDefinition definition = new GenericBeanDefinition();
-        definition.setBeanClass(MockServiceFactoryBean.class);
+        for (String dependencyName : dependencies.keySet()) {
+            Class<?> targetClass = dependencies.get(dependencyName);
+            Object mock = serviceMocks.computeIfAbsent(targetClass, k -> mock(targetClass));
 
-        ConstructorArgumentValues args = new ConstructorArgumentValues();
-        args.addGenericArgumentValue(mockService);
-        args.addGenericArgumentValue(serviceClass);
-        definition.setConstructorArgumentValues(args);
-
-        registry.registerBeanDefinition(beanName, definition);
+            registerBeanDefinition(registry, getBeanName(targetClass), targetClass, mock);
+        }
     }
 
-    private void registerControllerBean(
+    private void registerControllerDefinition(
             BeanDefinitionRegistry registry,
             Class<?> controllerClass,
             Map<String, Class<?>> dependencies
     ) {
-        String controllerBeanName = getBeanName(controllerClass);
-
         GenericBeanDefinition definition = new GenericBeanDefinition();
         definition.setBeanClass(controllerClass);
 
-        Constructor<?> constructor = controllerClass.getDeclaredConstructors()[0];
         ConstructorArgumentValues args = new ConstructorArgumentValues();
-        Parameter[] parameters = constructor.getParameters();
 
-        for (int i = 0; i < parameters.length; i++) {
-            Parameter param = parameters[i];
-            Class<?> paramType = param.getType();
+        for (Parameter parameter : controllerClass.getDeclaredConstructors()[0].getParameters()) {
+            Class<?> type = parameter.getType();
 
-            if (dependencies.containsKey(param.getName())) {
-                args.addIndexedArgumentValue(i, new RuntimeBeanReference(getBeanName(paramType)));
+            if (dependencies.containsValue(type)) {
+                args.addGenericArgumentValue(new RuntimeBeanReference(getBeanName(type)));
             }
-
-            definition.setConstructorArgumentValues(args);
         }
 
-        registry.registerBeanDefinition(controllerBeanName, definition);
+        definition.setConstructorArgumentValues(args);
+        registry.registerBeanDefinition(getBeanName(controllerClass), definition);
     }
 
     private String getBeanName(Class<?> clazz) {
@@ -165,29 +124,57 @@ public class RestControllerTestInitializer implements ApplicationContextInitiali
         return Character.toLowerCase(simpleName.charAt(0)) + simpleName.substring(1);
     }
 
+    private void registerBeanDefinition(
+            BeanDefinitionRegistry registry,
+            String beanName,
+            Class<?> targetType,
+            Object instance
+    ) {
+        GenericBeanDefinition definition = new GenericBeanDefinition();
+        definition.setBeanClass(MockServiceFactoryBean.class);
+
+        ConstructorArgumentValues args = new ConstructorArgumentValues();
+        args.addGenericArgumentValue(instance);
+        args.addGenericArgumentValue(targetType);
+        definition.setConstructorArgumentValues(args);
+
+        registry.registerBeanDefinition(beanName, definition);
+    }
+
+    private Set<Class<?>> findRestControllerClasses() {
+        Set<Class<?>> results = new HashSet<>();
+        ClassPathScanningCandidateComponentProvider scanner =
+                new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(RestController.class));
+
+        for (BeanDefinition beanDefinition : scanner.findCandidateComponents(BASE_PACKAGE)) {
+            try {
+                results.add(Class.forName(beanDefinition.getBeanClassName()));
+            } catch (ClassNotFoundException e) {
+                throw new TestContextClassNotFoundException(e);
+            }
+        }
+        return results;
+    }
+
     public static class MockServiceFactoryBean implements FactoryBean<Object> {
 
-        private final Object mockInstance;
-        private final Class<?> serviceType;
+        private final Object instance;
+        private final Class<?> targetType;
 
-        public MockServiceFactoryBean(Object mockInstance, Class<?> serviceType) {
-            this.mockInstance = mockInstance;
-            this.serviceType = serviceType;
+        public MockServiceFactoryBean(Object instance, Class<?> targetType) {
+            this.instance = instance;
+            this.targetType = targetType;
         }
 
         @Override
         public Object getObject() {
-            return mockInstance;
+            return instance;
         }
 
         @Override
         public Class<?> getObjectType() {
-            return serviceType;
-        }
-
-        @Override
-        public boolean isSingleton() {
-            return true;
+            return targetType;
         }
     }
 }
