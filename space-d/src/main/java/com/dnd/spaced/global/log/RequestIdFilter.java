c8b9197508151fc1befc7a6d23656ed83c3303dc
apptie <appti@naver.com>
2025-04-02 14:47:41 +0900

    fix: 애플리케이션 로그 식별자를 UUID가 아닌 HTTP Request Message에서 조회하도록 변경

--- space-d/src/main/java/com/dnd/spaced/global/log/RequestIdFilter.java
+++ space-d/src/main/java/com/dnd/spaced/global/log/RequestIdFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.UUID;
 import org.slf4j.MDC;
@@ -14,14 +15,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class RequestIdFilter implements Filter {
 
+    private static final String REQUEST_ID_HEADER_NAME = "X-RequestID";
+
     @Override
     public void doFilter(
             ServletRequest servletRequest,
             ServletResponse servletResponse,
             FilterChain filterChain
     ) throws IOException, ServletException {
-        String uuid = UUID.randomUUID().toString();
-        MDC.put(LogConst.REQUEST_ID, uuid);
+        String requestId = ((HttpServletRequest)servletRequest).getHeader(REQUEST_ID_HEADER_NAME);
+        MDC.put(LogConst.REQUEST_ID, requestId);
 
         try {
             filterChain.doFilter(servletRequest, servletResponse);
