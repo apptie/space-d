020d26c620628acddea60fe385d4da7b47f8239e
apptie <appti@naver.com>
2025-04-14 23:16:45 +0900

    feat: 용어 삭제 관리자 서비스 기능 추가

---
+++ space-d/src/main/java/com/dnd/spaced/core/admin/application/event/listener/AdminWordEventListener.java
@@ -0,0 +1,25 @@
+package com.dnd.spaced.core.admin.application.event.listener;
+
+import com.dnd.spaced.core.admin.application.event.dto.DeletedWordEvent;
+import com.dnd.spaced.core.word.domain.repository.PronunciationRepository;
+import com.dnd.spaced.core.word.domain.repository.WordExampleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class AdminWordEventListener {
+
+    private final WordExampleRepository wordExampleRepository;
+    private final PronunciationRepository pronunciationRepository;
+
+    @EventListener
+    @Transactional(propagation = Propagation.MANDATORY)
+    public void listen(DeletedWordEvent event) {
+        wordExampleRepository.deleteAllBy(event.wordId());
+        pronunciationRepository.deleteAllBy(event.wordId());
+    }
+}
