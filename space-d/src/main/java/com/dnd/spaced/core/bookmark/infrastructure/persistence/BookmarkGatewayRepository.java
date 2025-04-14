d2d0ccdf30baacc13ba280dddbd717ad214b1ecc
apptie <appti@naver.com>
2025-04-15 01:48:29 +0900

    fix: 북마크 조회 시 용어 삭제 여부를 검증하도록 변경

--- space-d/src/main/java/com/dnd/spaced/core/bookmark/infrastructure/persistence/BookmarkGatewayRepository.java
+++ space-d/src/main/java/com/dnd/spaced/core/bookmark/infrastructure/persistence/BookmarkGatewayRepository.java
@@ -1,9 +1,11 @@
 package com.dnd.spaced.core.bookmark.infrastructure.persistence;
 
 import static com.dnd.spaced.core.bookmark.domain.QBookmark.bookmark;
+import static com.dnd.spaced.core.word.domain.QWord.word;
 
 import com.dnd.spaced.core.bookmark.domain.Bookmark;
 import com.dnd.spaced.core.bookmark.domain.repository.BookmarkRepository;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -26,20 +28,42 @@ public class BookmarkGatewayRepository implements BookmarkRepository {
 
     @Override
     public Optional<Bookmark> findBy(Long accountId, Long wordId) {
-        Bookmark result = queryFactory.selectFrom(bookmark)
-                                      .where(bookmark.accountId.eq(accountId), bookmark.wordId.eq(wordId))
-                                      .fetchOne();
+        BookmarkWithWord result = queryFactory.select(Projections.constructor(
+                                                      BookmarkWithWord.class,
+                                                      bookmark,
+                                                      word.deleted
+                                              ))
+                                              .from(bookmark)
+                                              .leftJoin(word).on(bookmark.wordId.eq(word.id))
+                                              .where(
+                                                      word.id.eq(wordId),
+                                                      bookmark.accountId.eq(accountId)
+                                              )
+                                              .fetchOne();
 
-        return Optional.ofNullable(result);
+        if (result == null || result.wordDeleted()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(result.bookmark);
     }
 
     @Override
     public boolean existsBy(Long accountId, Long wordId) {
-        Bookmark result = queryFactory.selectFrom(bookmark)
-                                      .where(bookmark.accountId.eq(accountId), bookmark.wordId.eq(wordId))
-                                      .fetchOne();
+        BookmarkWithWord result = queryFactory.select(Projections.constructor(
+                                                      BookmarkWithWord.class,
+                                                      bookmark,
+                                                      word.deleted
+                                              ))
+                                              .from(bookmark)
+                                              .leftJoin(word).on(bookmark.wordId.eq(word.id))
+                                              .where(
+                                                      word.id.eq(wordId),
+                                                      bookmark.accountId.eq(accountId)
+                                              )
+                                              .fetchOne();
 
-        return result != null;
+        return result != null && !result.wordDeleted;
     }
 
     @Override
@@ -51,11 +75,21 @@ public class BookmarkGatewayRepository implements BookmarkRepository {
 
     @Override
     public List<Bookmark> findAllBy(Long accountId, Long lastBookmarkId, Pageable pageable) {
-        return queryFactory.selectFrom(bookmark)
+        return queryFactory.select(Projections.constructor(
+                                   BookmarkWithWord.class,
+                                   bookmark,
+                                   word.deleted
+                           ))
+                           .from(bookmark)
+                           .leftJoin(word).on(word.id.eq(bookmark.wordId))
                            .where(bookmark.accountId.eq(accountId), ltLastBookmarkId(lastBookmarkId))
                            .orderBy(bookmark.id.desc())
                            .limit(pageable.getPageSize())
-                           .fetch();
+                           .fetch()
+                           .stream()
+                           .filter(BookmarkWithWord::isNotWordDeleted)
+                           .map(BookmarkWithWord::bookmark)
+                           .toList();
     }
 
     private BooleanExpression ltLastBookmarkId(Long lastBookmarkId) {
@@ -64,5 +98,12 @@ public class BookmarkGatewayRepository implements BookmarkRepository {
         }
 
         return bookmark.id.lt(lastBookmarkId);
+    }
+
+    public record BookmarkWithWord(Bookmark bookmark, boolean wordDeleted) {
+
+        public boolean isNotWordDeleted() {
+            return !wordDeleted();
+        }
     }
 }
