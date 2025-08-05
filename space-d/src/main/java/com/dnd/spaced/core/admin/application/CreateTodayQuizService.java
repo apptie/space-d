cbff82d623a713fa2284253caa25124033c8da5c
apptie <appti@naver.com>
2025-08-05 10:50:36 +0900

    refactor: CreateTodayQuizService 클래스 접근 제어자를 package-private으로 변경

--- space-d/src/main/java/com/dnd/spaced/core/admin/application/CreateTodayQuizService.java
+++ space-d/src/main/java/com/dnd/spaced/core/admin/application/CreateTodayQuizService.java
@@ -24,7 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class CreateTodayQuizService {
+class CreateTodayQuizService {
 
     private static final Long DEFAULT_WORD_METADATA_ID = 1L;
     private static final int REQUIRED_TODAY_QUIZ_WORD_COUNT = 4;
